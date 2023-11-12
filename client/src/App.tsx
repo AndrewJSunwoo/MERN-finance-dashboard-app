24a6846e32fdfee6e3cb9243feb09eb71eae3a0b
Andrewjsunwoo <Andrewjsunwoo@users.noreply.github.com>
2023-11-11 22:46:10 -0500

    mui config and theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,19 @@
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { BrowserRouter } from "react-router-dom";
+
 function App() {
-  return <></>;
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+        </ThemeProvider>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
