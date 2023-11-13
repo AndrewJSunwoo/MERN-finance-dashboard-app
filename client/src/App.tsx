c8c68a465ca37ab7703eb2377cb17b34cbc8c2cf
Andrewjsunwoo <Andrewjsunwoo@users.noreply.github.com>
2023-11-13 15:57:06 -0500

    added dashboard layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,12 @@ import { themeSettings } from "./theme";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // MUI
-import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 
 // Components
 import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -18,7 +20,7 @@ function App() {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
-              <Route path="/" element={<div>Dashboard</div>} />
+              <Route path="/" element={<Dashboard />} />
               <Route
                 path="/predictions"
                 element={<div>Predictions page</div>}
