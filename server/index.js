8decee4f00947c63cef11a5cd346431c3bf8f737
Andrewjsunwoo <Andrewjsunwoo@users.noreply.github.com>
2023-11-18 17:05:23 -0500

    Added transaction UI

--- server/index.js
+++ server/index.js
@@ -7,9 +7,11 @@ import helmet from "helmet";
 import morgan from "morgan";
 import productRoutes from "./routes/product.js";
 import kpiRoutes from "./routes/kpi.js";
+import transactionRoutes from "./routes/transaction.js";
 import Product from "./models/Product.js";
 import KPI from "./models/KPI.js";
-import { kpis, products } from "./data/data.js";
+import Transaction from "./models/Transaction.js";
+import { kpis, products, transactions } from "./data/data.js";
 
 /* Config */
 dotenv.config();
@@ -25,7 +27,7 @@ app.use(cors());
 /* Routes */
 app.use("/kpi", kpiRoutes);
 app.use("/product", productRoutes);
-
+app.use("/transaction", transactionRoutes);
 /* Mongoose setup */
 const PORT = process.env.PORT || 9000;
 mongoose
@@ -36,5 +38,6 @@ mongoose
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
     // Product.insertMany(products);
+    // Transaction.insertMany(transactions);
   })
   .catch((error) => console.log(`${error} did not connect`));
